80996fe8b1b6a2eaa6a35b6aafb5a09e7956f55a
Amey Kusurkar <amey1000@gmail.com>
2018-10-28 14:27:12 +0000

    Abstract out tile width from snake class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pdb
 import tkinter as tk
-from collections import deque
 
 from snake import Snake
 
@@ -19,7 +18,10 @@ the_snake = Snake((WIDTH/2, HEIGHT/2))
 
 def render_snake(the_snake):
     for x, y in the_snake.body:
-        canvas.create_rectangle(x, y, x + TILE_WIDTH, y + TILE_WIDTH)
+        coord_x, coord_y = x * TILE_WIDTH, y * TILE_WIDTH
+        canvas.create_rectangle(coord_x, coord_y,
+                                coord_x + TILE_WIDTH,
+                                coord_y + TILE_WIDTH)
 
 def game_loop():
     # pdb.set_trace()
--- snake.py
+++ snake.py
@@ -1,15 +1,17 @@
+from collections import deque
+
 class Snake:
     def __init__(self, coords):
-        self.body = [coords]
+        self.body = deque()
+        self.body.append(coords)
         self.direction = 'east'
 
     def move(self):
         if len(self.body) == 1:
              # import code; code.interact(local=dict(globals(), **locals()))
             (x, y) = self.body.pop()
-            self.body.append((x + TILE_WIDTH, y))
+            self.body.append((x + 1, y))
         else:
-            # need to pop_left here
-            _, *rest = self.body
-            (x, y) = rest[-1]
-            self.body.append(x + TILE_WIDTH, y)
+            self.body.popleft()
+            (x, y) = self.body[-1]
+            self.body.append(x + 1, y)
