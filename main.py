e2a98e78a5707f721e9a64608213a99bd14f0726
Amey Kusurkar <amey1000@gmail.com>
2018-10-28 14:19:55 +0000

    Move snake class into own file

---
+++ main.py
@@ -0,0 +1,32 @@
+import pdb
+import tkinter as tk
+from collections import deque
+
+from snake import Snake
+
+root = tk.Tk()
+
+WIDTH, HEIGHT = 500, 500
+
+FRAME_RATE = 250
+TILE_WIDTH = 25
+
+canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT)
+canvas.create_rectangle(250, 250, 275, 275, outline="black")
+canvas.pack()
+
+the_snake = Snake((WIDTH/2, HEIGHT/2))
+
+def render_snake(the_snake):
+    for x, y in the_snake.body:
+        canvas.create_rectangle(x, y, x + TILE_WIDTH, y + TILE_WIDTH)
+
+def game_loop():
+    # pdb.set_trace()
+    the_snake.move()
+    canvas.delete("all")
+    render_snake(the_snake)
+    root.after(FRAME_RATE, game_loop)
+
+root.after(FRAME_RATE, game_loop)
+root.mainloop()
--- snake.py
+++ snake.py
@@ -1,25 +1,4 @@
-import pdb
-import tkinter as tk
-
-
-
-from collections import deque
-
-root = tk.Tk()
-
-WIDTH, HEIGHT = 500, 500
-
-FRAME_RATE = 250
-TILE_WIDTH = 25
-
-canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT)
-canvas.create_rectangle(250, 250, 275, 275, outline="black")
-canvas.pack()
-
-
-# Logic
 class Snake:
-
     def __init__(self, coords):
         self.body = [coords]
         self.direction = 'east'
@@ -34,23 +13,3 @@ class Snake:
             _, *rest = self.body
             (x, y) = rest[-1]
             self.body.append(x + TILE_WIDTH, y)
-
-
-the_snake = Snake((WIDTH/2, HEIGHT/2))
-
-def render_snake(the_snake):
-    for x, y in the_snake.body:
-        canvas.create_rectangle(x, y, x + TILE_WIDTH, y + TILE_WIDTH)
-
-def game_loop():
-    # pdb.set_trace()
-    the_snake.move()
-    canvas.delete("all")
-    render_snake(the_snake)
-    root.after(FRAME_RATE, game_loop)
-
-root.after(FRAME_RATE, game_loop)
-root.mainloop()
-
-
-
