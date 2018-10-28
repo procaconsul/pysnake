1369aa3893895e7ae14b43a055653321f0eedaa2
Amey Kusurkar <amey1000@gmail.com>
2018-10-28 14:59:23 +0000

    Rename the_snake to snake
    This just makes more sense, Ivan

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ TILE_WIDTH = 25
 canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT)
 canvas.pack()
 
-the_snake = Snake((WIDTH/TILE_WIDTH/2, HEIGHT/TILE_WIDTH/2))
+snake = Snake((WIDTH/TILE_WIDTH/2, HEIGHT/TILE_WIDTH/2))
 
-def render_snake(the_snake):
-    for x, y in the_snake.body:
+def render_snake(snake):
+    for x, y in snake.body:
         coord_x, coord_y = x * TILE_WIDTH, y * TILE_WIDTH
         canvas.create_rectangle(coord_x, coord_y,
                                 coord_x + TILE_WIDTH,
@@ -26,9 +26,9 @@ def render_snake(the_snake):
 
 def game_loop():
     # pdb.set_trace()
-    the_snake.move()
+    snake.move()
     canvas.delete("all")
-    render_snake(the_snake)
+    render_snake(snake)
     root.after(FRAME_RATE, game_loop)
 
 root.after(FRAME_RATE, game_loop)
