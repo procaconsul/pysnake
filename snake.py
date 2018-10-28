f250cd1a21b5694da0515064e8d5b31177d80adf
Ivan Procaccini <ivanprocaccini905@gmail.com>
2018-10-28 15:23:32 +0000

    adding direction enumeration

--- snake.py
+++ snake.py
@@ -1,13 +1,23 @@
 from collections import deque
+from enum import Enum
+
+class Direction(Enum):
+    NORTH='north'
+    EAST='east'
+    SOUTH='south'
+    WEST='west'
 
 class Snake:
     def __init__(self, coords, env_width):
         self.body = deque()
         self.body.append(coords)
-        self.direction = 'east'
+        self.direction = Direction.EAST
         self.env_width = env_width
 
     def move(self):
         tail = self.body.popleft()
         x, y = self.body[-1] if len(self.body) else tail
-        self.body.append(((x + 1) % self.env_width, y))
+        if self.direction == Direction.EAST:
+            self.body.append(((x + 1) % self.env_width, y))
+       
+
