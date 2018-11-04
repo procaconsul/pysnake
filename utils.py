e70a239017e0ea74145f5197a3a94b8cbd535520
Amey Kusurkar <amey1000@gmail.com>
2018-11-04 19:27:42 +0000

    Refactor snake movement logic

--- utils.py
+++ utils.py
@@ -2,9 +2,12 @@ from enum import Enum
 from collections import namedtuple 
 
 class Direction(Enum):
-    NORTH = 1
-    EAST  = 2
-    SOUTH = -1
-    WEST  = -2
+    NORTH = 0
+    EAST  = 1
+    SOUTH = 2
+    WEST  = 3
+
+    def is_opposite(self, other):
+        return self.value + other.value % 2 == 0
 
 Point = namedtuple('Point', 'x, y')
