a48457ca0161a27f8336371cf4638ac1d6ddb103
Amey Kusurkar <amey1000@gmail.com>
2018-11-05 22:29:05 +0000

    Fix bug in modulus calculation for checking opposite direction

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from collections import namedtuple 
+from collections import namedtuple
 
 class Direction(Enum):
     NORTH = 0
@@ -8,6 +8,6 @@ class Direction(Enum):
     WEST  = 3
 
     def is_opposite(self, other):
-        return self.value + other.value % 2 == 0
+        return (self.value + other.value) % 2 == 0
 
 Point = namedtuple('Point', 'x, y')
